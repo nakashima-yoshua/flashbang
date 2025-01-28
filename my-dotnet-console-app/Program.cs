d2c102142f13878aa75a3336970f92277afb73e7
Yoshua Nakashima <yoshua.n.j@gmail.com>
2025-01-28 19:50:22 +0900

    net 9プロジェクト追加

---
+++ my-dotnet-console-app/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyDotNetConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
