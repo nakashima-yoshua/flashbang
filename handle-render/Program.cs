e426370ad20bba6d2e0363571c63dabbc714752d
Yoshua Nakashima <yoshua.n.j@gmail.com>
2025-01-30 23:10:21 +0900

    リスト対応

--- handle-render/Program.cs
+++ handle-render/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
 
-namespace my_dotnet_console_app
+namespace handleRender
 {
     /// <summary>
     /// テンプレート処理を行うクラス
@@ -36,39 +36,26 @@ namespace my_dotnet_console_app
 
             // XMLファイルを読み込む
             XDocument xmlDoc = XDocument.Load(xmlFilePath);
-            var xmlData = new Dictionary<string, object>();
-            foreach (var element in xmlDoc.Root.Elements())
-            {
-                if (element.HasElements)
-                {
-                    var subElements = new Dictionary<string, string>();
-                    foreach (var subElement in element.Elements())
-                    {
-                        subElements[subElement.Name.LocalName] = subElement.Value;
-                    }
-                    xmlData[element.Name.LocalName] = subElements;
-                }
-                else
-                {
-                    xmlData[element.Name.LocalName] = element.Value;
-                }
-            }
+            var xmlData = ParseElement(xmlDoc.Root);
 
             // Handlebars テンプレートをコンパイル
             var template = Handlebars.Compile(templateContent);
 
             // テンプレートにデータを適用
             string result = template(xmlData);
-
-            // 結果を txt ファイルに出力
-            string outputFilePath = Path.ChangeExtension(templateFilePath, ".txt");
-            File.WriteAllText(outputFilePath, result);
-
-            Console.WriteLine($"Output written to {outputFilePath}");
+            Console.Write(result);
 
             // 生成されたコードがC#かを判定
             bool isCSharpCode = IsCSharpCode(result);
             Console.WriteLine($"Is the generated code C#? {isCSharpCode}");
+
+            string extension = isCSharpCode ? ".cs" : ".txt";
+
+            // 結果を txt ファイルに出力
+            string outputFilePath = Path.ChangeExtension(templateFilePath, extension);
+            File.WriteAllText(outputFilePath, result);
+
+            Console.WriteLine($"Output written to {outputFilePath}");
         }
 
         /// <summary>
@@ -81,6 +68,108 @@ namespace my_dotnet_console_app
             var syntaxTree = CSharpSyntaxTree.ParseText(code);
             var diagnostics = syntaxTree.GetDiagnostics();
             return !diagnostics.Any(diagnostic => diagnostic.Severity == DiagnosticSeverity.Error);
+        }
+
+
+        /// <summary>
+        /// XML要素を再帰的に解析し、辞書に格納する
+        /// </summary>
+        /// <param name="element">XML要素</param>
+        /// <returns>辞書形式のデータ</returns>
+        static Dictionary<string, object> ParseElement(XElement element)
+        {
+            var result = new Dictionary<string, object>();
+
+            // 子要素をグループ化（同じ名前の要素が複数あるかチェック）
+            var groupedElements = element.Elements().GroupBy(e => e.Name.LocalName);
+
+            foreach (var group in groupedElements)
+            {
+                if (group.Count() > 1)
+                {
+                    // 同じタグ名の要素が複数ある場合（リストとして扱う）
+                    var list = new List<object>();
+                    foreach (var item in group)
+                    {
+                        if (item.HasElements)
+                        {
+                            // 子要素を持つ場合は辞書として格納
+                            list.Add(ParseElement(item));
+                        }
+                        else
+                        {
+                            // 単純な値の場合は文字列として格納
+                            list.Add(item.Value);
+                        }
+                    }
+                    result[group.Key] = list;
+                }
+                else
+                {
+                    // 単一の要素
+                    var child = group.First();
+                    if (child.HasElements)
+                    {
+                        result[child.Name.LocalName] = ParseElement(child);
+                    }
+                    else
+                    {
+                        result[child.Name.LocalName] = child.Value;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        class ObjectCollection : List<object>
+        {
+            public override string ToString()
+            {
+
+                return base.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 辞書を再帰的に出力する（デバッグ用）
+        /// </summary>
+        /// <param name="dict">出力する辞書</param>
+        /// <param name="indent">インデントの深さ</param>
+        static void PrintDictionary(Dictionary<string, object> dict, int indent)
+        {
+            foreach (var kvp in dict)
+            {
+                Console.Write(new string(' ', indent * 2));
+                Console.Write($"{kvp.Key}: ");
+
+                if (kvp.Value is Dictionary<string, object> nestedDict)
+                {
+                    Console.WriteLine();
+                    PrintDictionary(nestedDict, indent + 1);
+                }
+                else if (kvp.Value is List<object> list)
+                {
+                    Console.WriteLine();
+                    foreach (var item in list)
+                    {
+                        Console.Write(new string(' ', (indent + 1) * 2));
+                        if (item is Dictionary<string, object> itemDict)
+                        {
+                            Console.WriteLine();
+                            PrintDictionary(itemDict, indent + 2);
+                        }
+                        else
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(kvp.Value);
+                }
+            }
         }
     }
 }
